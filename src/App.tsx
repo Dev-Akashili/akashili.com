2f4dc3d3746c9887be7b2d5190885405ceb996a6
Chukwuemeka Akashili <94780632+Dev-Akashili@users.noreply.github.com>
2024-01-19 13:34:55 +0000

    Remove home page ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { Contact } from "./pages/Contact";
 import { Home } from "./pages/Home";
 
 export const App = () => {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </>
   );
 };
